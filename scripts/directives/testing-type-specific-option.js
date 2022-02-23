43f1326a263506d914c38af2e8a2a401448177e2
Paul Jaffre <jaffrepaul@gmail.com>
2022-02-23 17:50:41 -0500

    Update scripts/directives/testing-type-specific-option.js
    Co-authored-by: Ben Alman <cowboy@rj3.net>

--- scripts/directives/testing-type-specific-option.js
+++ scripts/directives/testing-type-specific-option.js
@@ -10,16 +10,14 @@ function processNode(node, { error }) {
     )
   }
 
-  const message = `Depending on which Cypress [test type](/guides/overview/choosing-testing-type)
-  you are using, you can configure your \`OPTION_HERE\` accordingly.`
-
   return endent`
     <Alert type="info">
 
     <strong class="alert-header"><Icon name="cogs"></Icon> Support File, Per Test
     Type</strong>
 
-    ${message.replace('OPTION_HERE', option)}
+    Depending on which Cypress [test type](/guides/overview/choosing-testing-type)
+    you are using, you can configure your \`${option}\` accordingly.
 
     - [component](/guides/references/configuration#component)
     - [e2e](/guides/references/configuration#e2e)
